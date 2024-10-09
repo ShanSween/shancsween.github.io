0d9d0a5aef4cd26dc8c2647324c7cca56a255a61
ShanSween <shancsween@gmail.com>
2024-10-09 17:00:49 -0600

    secondCommit

--- the_DDS_Project/afterHoursFee.test.js
+++
@@ -1,7 +0,0 @@
-const {afterHours} = require("./afterHoursFee.js");
-
-test('What is After Hours Fee?', () => {
-    const feeResult = afterHours(1645);
-    
-    expect(feeResult).toEqual({"afterHoursFee": 10});
-});
